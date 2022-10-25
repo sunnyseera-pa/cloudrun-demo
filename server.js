59fbb81873d008eaa4fd997bf3fc5092d1f66d53
sunnyseera-pa <103260912+sunnyseera-pa@users.noreply.github.com>
2022-10-25 16:46:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('A new connection has been made')
-    res.end('Hello Google')
+    res.end('Hello AWS!')
 })
 
 // Listen on a port 
